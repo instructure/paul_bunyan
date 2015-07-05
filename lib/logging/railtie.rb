4f82773ae040ff06b7face988b81e55a3c690cf9
Tyler Pickett <tpickett@instructure.com>
2015-07-05 10:12:12 -0600

    make Railtie setup the log subscriptions we care about
    Refs: SIS-1204
    Change-Id: Iff494fe048e58acae27694a9b68c4abab6cc7107

--- lib/logging/railtie.rb
+++ lib/logging/railtie.rb
@@ -7,6 +7,26 @@ module Logging
       STDOUT.sync = true
       Rails.logger = Logging.set_logger(STDOUT)
       Logging.logger.level = ENV['LOG_LEVEL'] || ::Rails.application.config.log_level || "INFO"
+
+      unsubscribe_default_log_subscribers
+      LogSubscriber.subscribe_to_events
+    end
+
+    def self.unsubscribe_default_log_subscribers
+      LogSubscriber.event_patterns.each do |pattern|
+        ActiveSupport::Notifications.notifier.listeners_for(pattern).each do |listener|
+          conditionally_unsubscribe(listener)
+        end
+      end
+    end
+
+    private
+
+    def self.conditionally_unsubscribe(listener)
+      delegate = listener.instance_variable_get(:@delegate)
+      if delegate.class == ActionController::LogSubscriber
+        ActiveSupport::Notifications.unsubscribe(listener)
+      end
     end
   end
 end
---
+++ spec/lib/logging/railtie_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+module Logging
+  RSpec.describe Railtie do
+    describe '.unsubscribe_default_log_subscribers' do
+      before do
+        @action_controller_subscriber = ActiveSupport::LogSubscriber.subscribers.find{|s|
+          s.class == ActionController::LogSubscriber
+        }
+      end
+
+      after do
+        # replace any subscriptions we may have blown away so the next test
+        # can be assured of a clean slate
+        ActionController::LogSubscriber.attach_to(
+          :action_controller,
+          @action_controller_subscriber
+        )
+      end
+
+      it 'must remove the ActionController::LogSubscriber subscription to process_action' do
+        expect(subscriber_classes_for('process_action.action_controller')).
+          to_not include ActionController::LogSubscriber
+      end
+
+      it 'must leave the ActionController::LogSubscriber subscription to deep_munge.action_controller in place' do
+        # I don't expect that we'll ever care to unsubcribe the logger
+        # non-event so we'll use it as a check to ensure we don't
+        # clobber all of the listeners, only the ones we care about
+        expect(subscriber_classes_for('logger.action_controller')).
+          to include ActionController::LogSubscriber
+      end
+    end
+  end
+end
