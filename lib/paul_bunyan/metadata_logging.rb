ec6d7eedbf090644f4616b88c52e221545796bb3
Michael Brewer-Davis <mbd@instructure.com>
2018-01-19 08:32:11 -0600

    add delegation of more metadata methods

--- lib/paul_bunyan/metadata_logging.rb
+++ lib/paul_bunyan/metadata_logging.rb
@@ -8,6 +8,18 @@ module PaulBunyan
       formatter.with_metadata(metadata) { yield self }
     end
 
+    def add_metadata(metadata)
+      formatter.add_metadata(metadata)
+    end
+
+    def remove_metadata(metadata)
+      formatter.remove_metadata(metadata)
+    end
+
+    def current_metadata
+      formatter.current_metadata
+    end
+
     def flush
       clear_metadata!
       super if defined?(super)
