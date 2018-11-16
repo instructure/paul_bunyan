4e8ec657596016dfb3e9d6aa7ef6ba891e0c3719
Tyler Pickett <tpickett@instructure.com>
2018-11-16 14:46:01 -0600

    Make metadata logging module more resilient
    We're having to do this because the formatter can be changed after this
    module has been extended onto a logger causing any metadata calls to
    fail. This guards against any formatter being swapped into one of the
    loggers.
    
    Change-Id: I7e7534301211fe8d77b41b8aabbea10da907b5f7
    Reviewed-on: https://gerrit.instructure.com/172750
    Tested-by: Jenkins
    Reviewed-by: Neil Gupta <ngupta@instructure.com>
    QA-Review: Tyler Pickett <tpickett@instructure.com>
    Product-Review: Tyler Pickett <tpickett@instructure.com>

--- lib/paul_bunyan/metadata_logging.rb
+++ lib/paul_bunyan/metadata_logging.rb
@@ -1,23 +1,31 @@
 module PaulBunyan
   module MetadataLogging
     def clear_metadata!
-      formatter.clear_metadata!
+      formatter.clear_metadata! if formatter.respond_to?(:clear_metadata!)
     end
 
     def with_metadata(metadata)
-      formatter.with_metadata(metadata) { yield self }
+      if formatter.respond_to?(:with_metadata)
+        formatter.with_metadata(metadata) { yield self }
+      else
+        yield self
+      end
     end
 
     def add_metadata(metadata)
-      formatter.add_metadata(metadata)
+      formatter.add_metadata(metadata) if formatter.respond_to?(:add_metadata)
     end
 
     def remove_metadata(metadata)
-      formatter.remove_metadata(metadata)
+      formatter.remove_metadata(metadata) if formatter.respond_to?(:remove_metadata)
     end
 
     def current_metadata
-      formatter.current_metadata
+      if formatter.respond_to?(:current_metadata)
+        return formatter.current_metadata
+      else
+        return {}
+      end
     end
 
     def flush
--- spec/lib/paul_bunyan/metadata_logging_spec.rb
+++ spec/lib/paul_bunyan/metadata_logging_spec.rb
@@ -1,62 +1,108 @@
 require 'spec_helper'
 
 describe PaulBunyan::MetadataLogging do
-  subject { Object.new }
-  let(:formatter) { double('formatter') }
+  context "with a formatter that supports metadata" do
+    subject { Object.new }
+    let(:formatter) { double('formatter') }
 
-  before do
-    subject.extend(PaulBunyan::MetadataLogging)
-    allow(subject).to receive(:formatter).and_return(formatter)
-  end
-
-  it 'must delegate clear_metadata! to the formatter' do
-    expect(formatter).to receive(:clear_metadata!)
-    subject.clear_metadata!
-  end
-
-  it 'must delegate with_metadata to the formatter' do
-    expect(formatter).to receive(:with_metadata).with(foo: 'bar').and_yield
-    subject.with_metadata(foo: 'bar') do |logger|
-      expect(subject).to eq logger
+    before do
+      subject.extend(PaulBunyan::MetadataLogging)
+      allow(subject).to receive(:formatter).and_return(formatter)
     end
-  end
 
-  it 'must delegate add_metadata to the formatter' do
-    expect(formatter).to receive(:add_metadata).with(foo: 'bar')
-    subject.add_metadata(foo: 'bar')
-  end
-
-  it 'must delegate current_metadata to the formatter' do
-    expect(formatter).to receive(:current_metadata).and_return({})
-    subject.current_metadata
-  end
-
-  it 'must delegate remove_metadata to the formatter' do
-    expect(formatter).to receive(:remove_metadata).with(foo: 'bar')
-    subject.remove_metadata(foo: 'bar')
-  end
-
-  context '#flush' do
-    it 'clears metadata on the formatter' do
+    it 'must delegate clear_metadata! to the formatter' do
       expect(formatter).to receive(:clear_metadata!)
-      subject.flush
+      subject.clear_metadata!
     end
 
-    it 'sends flush to a parent' do
-      klass = Class.new
-      klass.class_eval do
-        def flush
-          flush_behavior()
-        end
+    it 'must delegate with_metadata to the formatter' do
+      expect(formatter).to receive(:with_metadata).with(foo: 'bar').and_yield
+      subject.with_metadata(foo: 'bar') do |logger|
+        expect(subject).to eq logger
+      end
+    end
+
+    it 'must delegate add_metadata to the formatter' do
+      expect(formatter).to receive(:add_metadata).with(foo: 'bar')
+      subject.add_metadata(foo: 'bar')
+    end
+
+    it 'must delegate current_metadata to the formatter' do
+      expect(formatter).to receive(:current_metadata).and_return({})
+      subject.current_metadata
+    end
+
+    it 'must delegate remove_metadata to the formatter' do
+      expect(formatter).to receive(:remove_metadata).with(foo: 'bar')
+      subject.remove_metadata(foo: 'bar')
+    end
+
+    context '#flush' do
+      it 'clears metadata on the formatter' do
+        expect(formatter).to receive(:clear_metadata!)
+        subject.flush
       end
 
-      metadata_logger = klass.new
-      metadata_logger.extend(PaulBunyan::MetadataLogging)
-      allow(metadata_logger).to receive(:formatter).and_return(formatter)
-      allow(formatter).to receive(:clear_metadata!)
+      it 'sends flush to a parent' do
+        klass = Class.new
+        klass.class_eval do
+          def flush
+            flush_behavior()
+          end
+        end
 
-      expect(metadata_logger).to receive(:flush_behavior)
-      metadata_logger.flush
+        metadata_logger = klass.new
+        metadata_logger.extend(PaulBunyan::MetadataLogging)
+        allow(metadata_logger).to receive(:formatter).and_return(formatter)
+        allow(formatter).to receive(:clear_metadata!)
+
+        expect(metadata_logger).to receive(:flush_behavior)
+        metadata_logger.flush
+      end
+    end
+  end
+
+  context "with a formatter that does not support metadata" do
+    subject { Object.new }
+    let(:formatter) { PaulBunyan::TextFormatter.new }
+
+    before do
+      subject.extend(PaulBunyan::MetadataLogging)
+      allow(subject).to receive(:formatter).and_return(formatter)
+    end
+
+    it 'must not call clear_metadata! on the formatter' do
+      expect(formatter).to_not respond_to(:clear_metadata!)
+      subject.clear_metadata!
+    end
+
+    it 'must delegate with_metadata to the formatter' do
+      expect(formatter).to_not respond_to(:with_metadata)
+      subject.with_metadata(foo: 'bar') do |logger|
+        expect(subject).to eq logger
+      end
+    end
+
+    it 'must delegate add_metadata to the formatter' do
+      expect(formatter).to_not respond_to(:add_metadata)
+      subject.add_metadata(foo: 'bar')
+    end
+
+    it 'must delegate current_metadata to the formatter' do
+      expect(formatter).to_not respond_to(:current_metadata)
+      subject.current_metadata
+    end
+
+    it 'must delegate remove_metadata to the formatter' do
+      expect(formatter).to_not respond_to(:remove_metadata)
+      subject.remove_metadata(foo: 'bar')
+    end
+
+    context '#flush' do
+      it 'clears metadata on the formatter' do
+        expect(formatter).to_not respond_to(:clear_metadata!)
+        subject.flush
+      end
     end
   end
 end
